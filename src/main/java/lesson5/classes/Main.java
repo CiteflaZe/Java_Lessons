ce852ed8516116b360b05d5d9579f1a1b5a496a2
CiteflaZe <citeflaze@gmail.com>
2019-09-20 20:23:21 +0300

    Aplied changes to User

--- src/main/java/lesson5/classes/Main.java
+++ src/main/java/lesson5/classes/Main.java
@@ -1,9 +1,13 @@
 package lesson5.classes;
 
+import lesson5.classes.User.Address;
+
 public class Main {
 
     public static void main(String[] args) {
         Car car = new Car("BMW", 8, 3);
+
+        Address address = new Address(5);
 
     }
 
