c09dfa9f97eb766bb3bca0ccafe3caa4c13d71dd
CiteflaZe <citeflaze@gmail.com>
2019-09-21 19:03:06 +0300

    Added interface StudentListSearch for search functionality

--- src/main/java/hometask4/StudentListSearch.java
+++ src/main/java/hometask4/StudentListSearch.java
@@ -1,4 +1,13 @@
 package hometask4;
 
 public interface StudentListSearch {
+
+    String searchByFaculty(String faculty);
+
+    String searchByFacultyAndStudyYear(String faculty, int year);
+
+    String searchStudentsBornAfterDate();
+
+    String searchByStudyGroup(String studyGroup);
+
 }
