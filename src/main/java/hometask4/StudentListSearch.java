62ba2c1ea0eb732c96f43e447a84ca82f9e34b0b
CiteflaZe <citeflaze@gmail.com>
2019-09-21 20:58:51 +0300

    Applied minor change for search interface

--- src/main/java/hometask4/StudentListSearch.java
+++ src/main/java/hometask4/StudentListSearch.java
@@ -1,12 +1,14 @@
 package hometask4;
 
+import java.time.LocalDate;
+
 public interface StudentListSearch {
 
     String searchByFaculty(String faculty);
 
     String searchByFacultyAndStudyYear(String faculty, int year);
 
-    String searchStudentsBornAfterDate();
+    String searchStudentsBornAfterDate(LocalDate date);
 
     String searchByStudyGroup(String studyGroup);
 
